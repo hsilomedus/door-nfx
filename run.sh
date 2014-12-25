10a76286f553cc229ac293412240e3e1e3c021c8
Pance Cavkovski <pance.cavkovski@netcetera.com>
2014-12-25 01:51:32 +0100

    Run script fix.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 echo ----------------------------------------------------------------------
-sudo java -cp mantarget:lib/pi4j-core.jar mk.hsilomedus.doornfx.Main
+fbcp & sudo java -cp mantarget:lib/pi4j-core.jar mk.hsilomedus.doornfx.Main
+killall fbcp
 echo ----------------------------------------------------------------------
 
