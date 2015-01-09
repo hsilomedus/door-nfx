2cc7ff3fbb95f03b477d315a8bbadf52397a21ef
Pance Cavkovski <pance.cavkovski@netcetera.com>
2015-01-09 03:07:21 +0100

    Removed comments.

--- src/main/java/mk/hsilomedus/doornfx/Pi4JInputHandler.java
+++ src/main/java/mk/hsilomedus/doornfx/Pi4JInputHandler.java
@@ -1,14 +1,8 @@
 package mk.hsilomedus.doornfx;
+
 import mk.hsilomedus.pn532.IPN532Interface;
 import mk.hsilomedus.pn532.PN532;
 import mk.hsilomedus.pn532.PN532I2C;
-
-/*
- * Copyright (C) 2014 by Netcetera AG. All rights reserved. The copyright to the computer program(s)
- * herein is the property of Netcetera AG, Switzerland. The program(s) may be used and/or copied
- * only with the written permission of Netcetera AG or in accordance with the terms and conditions
- * stipulated in the agreement/contract under which the program(s) have been supplied.
- */
 
 public class Pi4JInputHandler implements Runnable {
 
